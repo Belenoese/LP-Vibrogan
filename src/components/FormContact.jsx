4df604aef20089a16de17d52a0b0822dc7d5d630
Belen <belen.neira05@gmail.com>
2024-01-02 17:00:30 -0300

    creating modal for success or error message on form submission

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -1,14 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import '../styles/FormContact.css'
+import emailjs from '@emailjs/browser';
+import SuccessModal from './SuccessModal';
 
 function FormContact() {
+
+  const formRef = useRef(null)
 
   const [inputValues, setInputValues] = useState({
     name: '',
     email: '',
     subject: '',
     message: '',
+    phone: '',
   });
+
+  const [showModal, setShowModal] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
   
   const handleInput = (fieldName, value) => {
     setInputValues({
@@ -17,13 +25,54 @@ function FormContact() {
     });
   };
 
+  const sendEmail = (e) => {
+    e.preventDefault(); 
+
+    
+    const emailData = {
+      from_name: inputValues.name,
+      from_email: inputValues.email,
+      subject: inputValues.subject,
+      message: inputValues.message,
+      from_phone: inputValues.phone,
+    };
+
+     
+     emailjs.send('service_hogfald', 'template_0llftsc', emailData, '5-Eo1cif3agNlPblZ')
+     .then((response) => {
+      console.log('Correo enviado con éxito:', response);
+      if (formRef.current) {
+        formRef.current.reset();
+
+        setInputValues({
+          name: '',
+          email: '',
+          subject: '',
+          message: '',
+          phone: '',
+        });
+        setShowModal(true);
+        setIsSuccess(true);
+      }
+    })
+     .catch((error) => {
+      console.error('Error al enviar el correo:', error);
+      setShowModal(true);
+      setIsSuccess(false);
+     });
+ };
+
+ const closeModal = () => {
+  setShowModal(false); 
+  setIsSuccess(false);
+};
 
   return (
     <>
       <section className='sectionContact'>
         <h1 className='titleContact'>¿Qué podemos hacer por usted?</h1>
-        <p className='descriptionContact'>Estamos preparados para trabajar en un proyecto de cualquier complejidad, ya sea comercial o residencial.</p>
-        <form className='formContact'>
+        <p className='descriptionContact'>Estamos preparados para trabajar en proyectos de cualquier complejidad, ya sea comercial o residencial.</p>
+        <form className='formContact' onSubmit={sendEmail} ref={formRef}>
           <div className='inputGroup'>
             <label htmlFor='name' className='visuallyHidden'>Nombre:</label>
             <input type='text' id='name' name='name' placeholder='NOMBRE' required  onChange={(e) => handleInput('name', e.target.value)} />
@@ -44,7 +93,7 @@ function FormContact() {
 
           <div className='inputGroup'>
           <label htmlFor='phone' className='visuallyHidden'>Teléfono:</label>
-            <input type='text' id='phone' name='phone' placeholder='TELÉFONO' required />
+            <input type='text' id='phone' name='phone' placeholder='TELÉFONO' onChange={(e) => handleInput('phone', e.target.value)} />
           </div>
 
           <div id='message' className='inputGroup'>
@@ -62,6 +111,7 @@ function FormContact() {
             <button className='buttonFormContact' type='submit'>ENVIAR</button>
           </div>
         </form>
+        {showModal && <SuccessModal closeModal={closeModal} isSuccess={isSuccess} />}
       </section>
     </>
   )
---
+++ src/components/SuccessModal.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/SuccessModal.css';
+
+const SuccessModal = ({ closeModal, isSuccess }) => {
+  return (
+    <div className="modal-overlay">
+      <div className="success-modal">
+        <h2 className='titleModal'>{isSuccess ? '¡Formulario enviado! Gracias por comunicarte con nosotros.' : 'Error al enviar el formulario'}</h2>
+        <p className='textModal'>{isSuccess ? 'Estaremos en contacto contigo pronto.' : 'Por favor, inténtalo de nuevo más tarde.'}</p>
+        <button className='buttonModal' onClick={closeModal}>Cerrar</button>
+      </div>
+    </div>
+  );
+};
+
+export default SuccessModal;
