761086106d1c9997923bb7784148747ad08cbcf1
Belen <belen.neira05@gmail.com>
2024-04-02 16:42:41 -0300

    adjusting the load of the components asynchronously

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Products from './components/Products';
-import Contact from './components/Contact';
-import Navigation from './components/Navigation'; 
-import ProductTable from './components/ProductTable';
-import Location from './components/location';
+import Navigation from './components/Navigation';
+
+
+const Home = React.lazy(() => import('./components/Home'));
+const About = React.lazy(() => import('./components/About'));
+const Products = React.lazy(() => import('./components/Products'));
+const Contact = React.lazy(() => import('./components/Contact'));
+const ProductTable = React.lazy(() => import('./components/ProductTable'));
+const Location = React.lazy(() => import('./components/location'));
 
 function App() {
-
   return (
     <div>
       <Navigation />
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/location" element={<Location />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/table/:category" element={<ProductTable />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
+    
+      <Suspense fallback={<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/location" element={<Location />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/table/:category" element={<ProductTable />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
