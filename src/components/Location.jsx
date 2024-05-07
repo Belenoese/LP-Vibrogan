357cf7c23bcd29a56dce06b270a1f9c83c3754b1
Belen <belen.neira05@gmail.com>
2024-05-07 16:58:39 -0400

    favicon path update

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -19,7 +19,7 @@ function Location() {
           <img loading='lazy' className='imgLocation' src={ImgSecondLocation} />
       </section>
       <section className='sectionMapsLocation'>
-      <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48212.09164528935!2d-72.43775965136717!3d-37.387227599999974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x966bdf0073bce39b%3A0x4826de02ab8e80a5!2svibrogan!5e1!3m2!1ses-419!2scl!4v1710450657220!5m2!1ses-419!2scl' style={{ border: 0, width: '100%', height: '100%' }} allowfullscreen='' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>
+      <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48212.09164528935!2d-72.43775965136717!3d-37.387227599999974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x966bdf0073bce39b%3A0x4826de02ab8e80a5!2svibrogan!5e1!3m2!1ses-419!2scl!4v1710450657220!5m2!1ses-419!2scl' style={{ border: 0, width: '100%', height: '100%' }} allowFullScreen='' loading='lazy' referrerPolicy='no-referrer-when-downgrade'></iframe>
       </section>
       <Footer />
     </>
